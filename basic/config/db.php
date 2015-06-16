44085911192079f42c99e067274a6412a99d1d83
xami <xami520@qq.com>
2015-06-16 05:14:27 -0400

    db

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '555aaa@@@mysql',
     'charset' => 'utf8',
 ];
